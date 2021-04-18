93e1f20612e1264845ef45f23c6db61cac8e516d
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-19 03:03:35 +0500

    add bootstrap styles to index.html for styling purposes

--- src/components/videoList.vue
+++ src/components/videoList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <VideoListItem v-for="video in videos" :key="video.id" v-bind:video="video">
+    <VideoListItem v-for="video in videos" :key="video.id" :video="video">
     </VideoListItem>
   </ul>
 </template>
